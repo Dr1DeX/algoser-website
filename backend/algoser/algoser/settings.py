a1d61e85a7582b81a7797fac92d4d6d5e235315f
nesterov-art <nesterov-art@yandex-team.ru>
2024-03-30 17:13:10 +0300

    Add DefaultApiRouter

--- backend/algoser/algoser/settings.py
+++ backend/algoser/algoser/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # REST
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+
+    # Main apps
+    
     'api.apps.ApiConfig',
     'posts.apps.PostsConfig'
 ]
@@ -124,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# REST SETTINGS
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
