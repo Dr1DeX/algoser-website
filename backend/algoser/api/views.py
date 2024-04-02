9734034db9172fe1ad4669630c6190a2c56f48a5
nesterov-art <nesterov-art@yandex-team.ru>
2024-04-02 18:41:41 +0300

    Change permission politic

--- backend/algoser/api/views.py
+++ backend/algoser/api/views.py
@@ -1,8 +1,10 @@
 from api.serializers import PostsSerializer
 from posts.models import Posts
 from rest_framework import viewsets
+from api.permissions import AuthorStaffOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostsSerializer
     queryset = Posts.objects.all()
+    permission_classes = (AuthorStaffOrReadOnly,)
