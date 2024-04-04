3116d33381f631788ce5a70da61af4ca0ffa6dcb
nesterov-art <nesterov-art@yandex-team.ru>
2024-04-04 18:39:42 +0300

    Add Dockerfile.nginx and fixed dev.stand

--- backend/algoser/api/urls.py
+++ backend/algoser/api/urls.py
@@ -5,8 +5,6 @@ from api import views
 
 app_name = 'api'
 
-
-
 urlpatterns = [
     path('v1/token', TokenObtainPairView.as_view(), name='token_obtain'),
     path('v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
